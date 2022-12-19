3b464f5af5d927b03d82917c2918eaf472b020cb
JorgeValenciadevops <jorgeladalcia180280@hotmail.com>
2022-12-18 19:47:10 -0500

    class_Ejercicio_1

--- temas.py
+++ temas.py
@@ -8,3 +8,21 @@
 "IF, ELIF  ELSE WHILE" "BREAK, CONTINUE"
 "FOR WITH ELSE "
 "MATCH"
+"KWARGS "
+
+
+def sumador(**kwargs):
+    # Operador ternario
+    # Operador ternario
+    inicial = kwargs['inicial'] if 'initial' in kwargs else 0
+    final = kwargs['final'] if 'final' in kwargs else 0  # Operador ternario
+    resultado = 0
+
+    for x in range(inicial, final+1):
+        resultado += x
+
+    return resultado
+
+
+sumador(inicial=10, final=15)
+"FUNCIONES LAMBDA"
